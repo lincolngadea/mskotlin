1f92478e5f22b32b3e230a1d00a70aa645bf8311
lincolngadea <lincoln@glinc.com.br>
2024-08-13 08:38:14 -0300

    shore: initial commit

---
+++ src/main/kotlin/br/com/lincolngadea/ms_kotlin_learning/MainApplication.kt
@@ -0,0 +1,11 @@
+package br.com.lincolngadea.ms_kotlin_learning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsKotlinLearningApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsKotlinLearningApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/lincolngadea/ms_kotlin_learning/MainApplication.kt
@@ -0,0 +1,13 @@
+package br.com.lincolngadea.ms_kotlin_learning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainApplication {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
